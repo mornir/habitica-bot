b95248662665885e74aeab1e6d47087ff680ed86
mornir <jeromepott@hotmail.com>
2021-03-28 11:06:27 +0200

    feat: add env

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
+  return new Response('Hello Switzerland!', {
     headers: { 'content-type': 'text/plain' },
   })
 }
