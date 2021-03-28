20e50da262607a8b564bf72d586ecc6b73006bd4
mornir <jeromepott@hotmail.com>
2021-03-28 11:15:53 +0200

    Copy existing script

--- index.js
+++ index.js
@@ -2,11 +2,37 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 /**
- * Respond with hello worker text
+ * Respond to the request
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello Switzerland!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+
+  try {
+
+    const { chat } = await request.json()
+
+    if(chat.uuid === "system") {
+      const content = { "content": chat.text }
+      await fetch(DISCORD_WEBHOOK_URL, {
+        body: JSON.stringify(content),
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      })
+
+      if(chat.info.type === "boss_damage" && parseInt(chat.info.userDamage) > 40)
+      await fetch(DISCORD_WEBHOOK_URL, {
+        body: JSON.stringify({"content": "https://tenor.com/view/damage-thats-alot-of-damage-jon-tron-gif-13054497"}),
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+      })
+    }
+
+    return new Response('OK')
+
+  } catch(error) {
+
+    console.error(error)
+    return new Response('OK')
+
+  }
 }
