e7c3923e05596005310c1824c37b6b5973684209
mornir <jeromepott@hotmail.com>
2021-04-07 23:40:30 +0200

    Refactor for better testing

---
+++ generateMessages.js
@@ -0,0 +1,56 @@
+import tenor from './tenor'
+
+export default function generateMessages(payload) {
+  const QUEST_INVITE_MSG =
+    '**Come and join us on a new quest! ⚔️  Quest starts in about 24 hours ⏳**'
+
+  const messages = []
+
+  function addMsg(msg, channel) {
+    messages.push({ msg, channel })
+  }
+
+  // If quest invite
+  if (payload.webhookType === 'questActivity') {
+    addMsg(QUEST_INVITE_MSG, 'quests')
+    addMsg(tenor.gif('quest_invite'), 'quests')
+    return messages
+  }
+
+  const { chat } = payload
+
+  if (chat.uuid !== 'system') return messages
+
+  if (chat.info.type === 'quest_start') {
+    addMsg(chat.text, 'quests')
+    addMsg(tenor.gif('quest_start'), 'quests')
+    return messages
+  }
+
+  if (chat.info.type === 'boss_defeated') {
+    addMsg(chat.text, 'quests')
+    addMsg(tenor.gif('quest_finish'), 'quests')
+    return messages
+  }
+
+  addMsg(chat.text, 'skills')
+
+  if (chat.info.type === 'boss_damage') {
+    const dmgSustained = parseFloat(chat.info.bossDamage)
+    if (dmgSustained > 8) {
+      addMsg(tenor.gif('dmg_sustained'), 'skills')
+      return messages
+    }
+
+    const dmgDealt = parseFloat(chat.info.userDamage)
+    if (dmgDealt > 40) {
+      addMsg(tenor.gif('dmg_high'), 'skills')
+    }
+
+    if (dmgDealt < 40 && dmgDealt > 20) {
+      addMsg(tenor.gif('dmg_mid'), 'skills')
+    }
+  }
+
+  return messages
+}
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const QUEST_INVITE_MSG =
-  '**Come and join us on a new quest! ⚔️  Quest starts in about 24 hours ⏳**'
+import generateMessages from './generateMessages'
 
-import tenor from './tenor'
-
-function postToDiscord(msg = '', channel = 'skills') {
+function postToDiscord({ msg = '', channel = 'skills' }) {
   const channels = {
     skills: DISCORD_WEBHOOK_URL,
     quests: DISCORD_QUESTS,
@@ -19,50 +16,9 @@ async function handleRequest(request) {
   try {
     const payload = await request.json()
 
-    const messages = generateMessages()
-
-    // If quest invite
-    if (payload.webhookType === 'questActivity') {
-      await postToDiscord(QUEST_INVITE_MSG, 'quests')
-      await postToDiscord(tenor.gif('quest_invite'), 'quests')
-      return new Response('OK')
-    }
-
-    const { chat } = payload
-
-    if (chat.uuid !== 'system') return new Response('OK')
-
-    if (chat.info.type === 'quest_start') {
-      await postToDiscord(chat.text, 'quests')
-      await postToDiscord(tenor.gif('quest_start'), 'quests')
-      return new Response('OK')
-    }
-
-    if (chat.info.type === 'boss_defeated') {
-      await postToDiscord(chat.text, 'quests')
-      await postToDiscord(tenor.gif('quest_finish'), 'quests')
-      return new Response('OK')
-    }
-
-    await postToDiscord(chat.text, 'skills')
-
-    if (chat.info.type === 'boss_damage') {
-      const dmgSustained = parseFloat(chat.info.bossDamage)
-      if (dmgSustained > 8) {
-        await postToDiscord(tenor.gif('dmg_sustained'), 'skills')
-        return new Response('OK')
-      }
-
-      const dmgDealt = parseFloat(chat.info.userDamage)
-      if (dmgDealt > 40) {
-        await postToDiscord(tenor.gif('dmg_high'), 'skills')
-      }
-
-      if (dmgDealt < 40 && dmgDealt > 20) {
-        await postToDiscord(tenor.gif('dmg_mid'), 'skills')
-      }
-    }
-
+    const messages = generateMessages(payload)
+    console.log(messages)
+    await Promise.all(messages.map((msg) => postToDiscord(msg)))
     return new Response('OK')
   } catch (error) {
     console.error(error)
