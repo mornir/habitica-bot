840267be9203b150813776e0adcd844f3412cc99
mornir <jeromepott@hotmail.com>
2021-04-21 21:21:51 +0200

    Add all pre written texts

--- data/customTexts.ts
+++ data/customTexts.ts
@@ -3,4 +3,20 @@ export default {
     '**Come and join us on a new quest! ⚔️  Quest starts in about 24 hours ⏳**',
   quest_non_participants:
     '**List of members who are not participating in the quest:**',
+  skills: {
+    healAll:
+      '💖 @user casts @skill for the party. Everyone recovers some health! 💖',
+    protectAura:
+      '🛡️ @user casts @skill for the party. Everyone takes less damage from their missed dailies and bad habits! 🛡️',
+    toolsOfTrade:
+      '💰 @user casts @skill for the party. The rate of earning gold and special items is increased for everyone! 💰',
+    intimidate:
+      '🛡️ @user casts @skill for the party. Everyone takes less damage from their missed dailies and bad habits! 🛡️',
+    valorousPresence:
+      '💪 @user casts @skill for the party. Everyone deals more damage to the boss! 💪',
+    earth:
+      '✨ @user casts @skill for the party. The rate of earning XP is increased for everyone! ✨',
+    mpheal:
+      '✨ @user casts @skill for the party. Everyone (except for mages) gains mana points! ✨',
+  },
 }
