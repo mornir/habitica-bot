2edfa64de505b41ac4d4c44bf350a12cef90ac85
mornir <jeromepott@hotmail.com>
2021-04-21 21:43:31 +0200

    Convert into object to save skill names

--- data/customTexts.ts
+++ data/customTexts.ts
@@ -4,19 +4,40 @@ export default {
   quest_non_participants:
     '**List of members who are not participating in the quest:**',
   skills: {
-    healAll:
-      '💖 @user casts @skill for the party. Everyone recovers some health! 💖',
-    protectAura:
-      '🛡️ @user casts @skill for the party. Everyone takes less damage from their missed dailies and bad habits! 🛡️',
-    toolsOfTrade:
-      '💰 @user casts @skill for the party. The rate of earning gold and special items is increased for everyone! 💰',
-    intimidate:
-      '🛡️ @user casts @skill for the party. Everyone takes less damage from their missed dailies and bad habits! 🛡️',
-    valorousPresence:
-      '💪 @user casts @skill for the party. Everyone deals more damage to the boss! 💪',
-    earth:
-      '✨ @user casts @skill for the party. The rate of earning XP is increased for everyone! ✨',
-    mpheal:
-      '✨ @user casts @skill for the party. Everyone (except for mages) gains mana points! ✨',
+    healAll: {
+      name: 'Blessing',
+      text:
+        '💖 @user casts @skill for the party. Everyone recovers some health! 💖',
+    },
+    protectAura: {
+      name: 'Protective Aura',
+      text:
+        '🛡️ @user casts @skill for the party. Everyone takes less damage from their missed dailies and bad habits! 🛡️',
+    },
+    toolsOfTrade: {
+      name: 'Tools of the Trade',
+      text:
+        '💰 @user casts @skill for the party. The rate of earning gold and special items is increased for everyone! 💰',
+    },
+    intimidate: {
+      name: 'Intimidating Gaze',
+      text:
+        '🛡️ @user casts @skill for the party. Everyone takes less damage from their missed dailies and bad habits! 🛡️',
+    },
+    valorousPresence: {
+      name: 'Valorous Presence',
+      text:
+        '💪 @user casts @skill for the party. Everyone deals more damage to the boss! 💪',
+    },
+    earth: {
+      name: 'Earthquake',
+      text:
+        '✨ @user casts @skill for the party. The rate of earning XP is increased for everyone! ✨',
+    },
+    mpheal: {
+      name: 'Ethereal Surge',
+      text:
+        '✨ @user casts @skill for the party. Everyone (except for mages) gains mana points! ✨',
+    },
   },
 }
