42bf2e4ae67a3686b97973e2576a3ca2b3b32915
mornir <jeromepott@hotmail.com>
2021-04-23 20:57:23 +0200

    Add remaining left over tests from Jest

--- cypress/integration/generateMessages.spec.js
+++ cypress/integration/generateMessages.spec.js
@@ -1,40 +1,95 @@
-describe('My First Test', () => {
-  it('testing', () => {
-    cy.request({
-      url: 'http://127.0.0.1:8787/',
-      method: 'POST',
-      body: {
-        group: {
-          id: '5f98dbe0-c888-4dbf-aa0e-fc317c9c8f8c',
-          name: 'super_squad ',
-        },
-        chat: {
-          flagCount: 0,
-          _id: '88a99b4c-d630-45b8-9162-da087f7e2456',
-          flags: {},
-          id: '88a99b4c-d630-45b8-9162-da087f7e2456',
-          text:
-            "`Marshmallow attacks Vice's Shade for 10.5 damage. Vice's Shade attacks party for 0.0 damage.`",
-          unformattedText:
-            "Marshmallow attacks Vice's Shade for 10.5 damage. Vice's Shade attacks party for 0.0 damage.",
-          info: {
-            type: 'boss_damage',
-            user: 'Marshmallow',
-            quest: 'vice1',
-            userDamage: '10.5',
-            bossDamage: '0.0',
-          },
-          timestamp: 1615134845081,
-          likes: {},
-          uuid: 'system',
-          groupId: '5f38dbe0-b949-4dbf-aa0e-fc317c9cbf8c',
-        },
-        webhookType: 'groupChatReceived',
-        user: { _id: '5454544ea-134d-4e37-bbdc-310351b35729' },
-      },
-    }).should((response) => {
-      cy.log(response)
-      expect(response.status).to.eq(200)
+import messages from '../fixtures/messages.json'
+import tenor from '../../data/tenor.ts'
+import customTexts from '../../data/customTexts'
+
+function checkChannel(messagesArray, channel) {
+  messagesArray.forEach((msg) => expect(msg.channel).to.equal(channel))
+}
+
+describe('Skills Channel', () => {
+  const channel = 'skills'
+
+  it('Less than 20 dmg to boss', () => {
+    const key = 'dmg_low'
+    cy.request('POST', '/', messages[key]).should(({ body }) => {
+      expect(body).to.have.length(1)
+      expect(body[0].msg).to.equal(messages[key].chat.text)
+      checkChannel(body, channel)
+    })
+  })
+
+  it('Dmg between 20 and 40 to boss', () => {
+    const key = 'dmg_mid'
+    cy.request('POST', '/', messages[key]).should(({ body }) => {
+      expect(body).to.have.length(2)
+      expect(body[0].msg).to.equal(messages[key].chat.text)
+      expect(body[1].msg).to.equal(tenor.gif(key))
+      checkChannel(body, channel)
+    })
+  })
+
+  it('Greater than 40 dmg to boss', () => {
+    const key = 'dmg_high'
+    cy.request('POST', '/', messages[key]).should(({ body }) => {
+      expect(body).to.have.length(2)
+      expect(body[0].msg).to.equal(messages[key].chat.text)
+      expect(body[1].msg).to.equal(tenor.gif(key))
+      checkChannel(body, channel)
+    })
+  })
+
+  it('Sustained more than 8 dmg', () => {
+    const key = 'dmg_sustained'
+    cy.request('POST', '/', messages[key]).should(({ body }) => {
+      expect(body).to.have.length(2)
+      expect(body[0].msg).to.equal(messages[key].chat.text)
+      expect(body[1].msg).to.equal(tenor.gif(key))
+      checkChannel(body, channel)
+    })
+  })
+})
+
+describe('Quest', () => {
+  const channel = 'quests'
+
+  it('Starts', () => {
+    const key = 'quest_start'
+
+    cy.request('POST', '/', messages[key]).should(({ body }) => {
+      expect(body).to.have.length(2)
+      expect(body[0].msg).to.equal(messages[key].chat.text)
+      expect(body[1].msg).to.equal(tenor.gif(key))
+      checkChannel(body, channel)
+    })
+  })
+
+  it.only('Sends invites', () => {
+    const key = 'quest_invite'
+
+    cy.request('POST', '/', messages[key]).should(({ body }) => {
+      expect(body).to.have.length(2)
+      expect(body[0].msg).to.equal(customTexts.quest_invite)
+      expect(body[1].msg).to.equal(tenor.gif(key))
+      checkChannel(body, channel)
+    })
+  })
+
+  it('Finishes', () => {
+    const key = 'quest_finish'
+    cy.request('POST', '/', messages[key]).should(({ body }) => {
+      expect(body).to.have.length(2)
+      expect(body[0].msg).to.equal(messages[key].chat.text)
+      expect(body[1].msg).to.equal(tenor.gif(key))
+      checkChannel(body, channel)
+    })
+  })
+})
+
+describe('Edge Cases', () => {
+  it('Non system messages', () => {
+    const key = 'non_system_msg'
+    cy.request('POST', '/', messages[key]).should(({ body }) => {
+      expect(body).to.have.length(0)
     })
   })
 })
